970483d829acc73b18e00fd775a775a3ecf4a3e3
IKEH1024 <pondau1024@gmail.com>
2025-04-09 12:48:25 +0900

    wip

--- AsyncDemoTests/SecondViewStateTests.swift
+++ AsyncDemoTests/SecondViewStateTests.swift
@@ -27,6 +27,7 @@ final class SecondViewStateTests: XCTestCase {
         try super.tearDownWithError()
     }
 
+    /// キャンセルなし, withMainSerialExecutor
     func testFetchRandomNumberButtonTapped() async throws {
         
         func test() async throws {
@@ -55,33 +56,40 @@ final class SecondViewStateTests: XCTestCase {
         }
     }
     
-//    func testFetchRandomNumberButtonTapped2() async throws {
-//        
-//        func test() async throws {
-//            // MARK: Given
-//            let testNumber = Int.random(in: 0...100)
-//            let apiClientMock = APIClientMock2()
-//            await apiClientMock.setRandomNumber(testNumber)
-//            sut = SecondViewState(apiClient: apiClientMock)
-//            XCTAssertNil(sut.number)
-//            
-//            // MARK: When
-//            try await sut.fetchRandomNumberButtonTapped()
-//            
-//            // MARK: Then
-//            await Task.yield()
-//            XCTAssertTrue(sut.isConnecting)
-//            try await sut.fetchRandomNumberTask?.value
-//            XCTAssertFalse(sut.isConnecting)
-//            XCTAssertEqual(sut.number, testNumber)
-//        }
-//        
-//        for _ in 0..<1000 {
-//            try await withMainSerialExecutor {
-//                try await test()
-//            }
-//        }
-//    }
+    /// キャンセルなし, continuation
+    func testFetchRandomNumberButtonTapped2() async throws {
+        
+        func test() async throws {
+            // MARK: Given
+            let testNumber = Int.random(in: 0...10000)
+            let apiClientMock = APIClientMock2()
+            sut = SecondViewState(apiClient: apiClientMock)
+            XCTAssertNil(sut.number)
+            
+            // MARK: When
+            sut.fetchRandomNumberButtonTapped()
+            while await apiClientMock.fetchRandomNumberContinuation == nil {
+                await Task.yield()
+            }
+            
+            // MARK: Then
+            XCTAssertTrue(sut.isConnecting)
+            
+            await apiClientMock.fetchRandomNumberContinuation?.resume(returning: testNumber)
+            await apiClientMock.setFetchRandomNumberContinuation(nil)
+            
+            try await sut.fetchRandomNumberTask?.value
+            
+            XCTAssertFalse(sut.isConnecting)
+            XCTAssertEqual(sut.number, testNumber)
+        }
+        
+        for _ in 0..<1000 {
+            try await withMainSerialExecutor {
+                try await test()
+            }
+        }
+    }
     
     // MARK: - Cancel
     
