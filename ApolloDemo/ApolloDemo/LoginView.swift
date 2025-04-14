7c22ae1d2e65d4dc07b0656a3ba816dabc8fe527
IKEH1024 <pondau1024@gmail.com>
2025-04-14 23:42:11 +0900

    wip

--- ApolloDemo/ApolloDemo/LoginView.swift
+++ ApolloDemo/ApolloDemo/LoginView.swift
@@ -39,16 +39,79 @@ final actor GitHubAPIClient {
 struct LoginView: View {
     
     let callbackURL = URL(string: "apollodemo://callback")!
+    @State private var sessionCode: String?
+    @State private var accessToken: String?
     
     var body: some View {
         Form {
+            LabeledContent("Code", value: sessionCode ?? "(nil)")
+            LabeledContent("AccessToken", value: accessToken ?? "(nil)")
+                        
             Button("Log in") {
                 let url = URL(string: "https://github.com/login/oauth/authorize?client_id=\(GitHubAPICredentials.clientID)&redirect_uri=\(callbackURL.absoluteString)")!
                 UIApplication.shared.open(url)
-                print(url.absoluteString)
             }
             .frame(maxWidth: .infinity, alignment: .center)
+            
+            Button("Fetch AccessToken") {
+                Task {
+                    do {
+                        guard let sessionCode else {
+                            return
+                        }
+                        self.accessToken = try await fetchAccessToken(sessionCode: sessionCode)
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+            .disabled(sessionCode == nil)
+            .frame(maxWidth: .infinity, alignment: .center)
         }
+        .onOpenURL { url in
+            print(url.absoluteString) // apollodemo://callback?code=8649aab669c51e066004
+            
+            guard
+                let components = URLComponents(url: url, resolvingAgainstBaseURL: false),
+                let queryItems = components.queryItems,
+                let sessionCode = queryItems.first(where: { $0.name == "code" })?.value
+            else {
+                fatalError("コールバックURLの値が不正です")
+            }
+            self.sessionCode = sessionCode
+        }
+    }
+    
+    private func fetchAccessToken(sessionCode: String) async throws -> String {
+        // リクエストの作成
+        let url = URL(string: "https://github.com/login/oauth/access_token")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        // ヘッダの設定
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        // bodyの設定
+        let bodyDict = [
+            "client_id": GitHubAPICredentials.clientID,
+            "client_secret": GitHubAPICredentials.clientSecret,
+            "code": sessionCode
+        ]
+        let body = try JSONSerialization.data(withJSONObject: bodyDict, options: [])
+        request.httpBody = body
+        
+        // リクエストの送信
+        let (data, _) = try await URLSession.shared.data(for: request)
+
+        // レスポンスのデコード
+        let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
+        guard
+            let jsonDict = jsonObject as? [String: Any],
+            let accessToken = jsonDict["access_token"] as? String
+        else {
+            fatalError("レスポンスのデコードに失敗")
+        }
+        
+        return accessToken
     }
 }
 
